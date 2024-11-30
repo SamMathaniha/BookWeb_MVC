bc506347e04290a5ce788c8e284bceb9e42e2c6c
SamMathaniha <mathanihasam@gmail.com>
2024-11-30 14:38:36 +0530

    View category table

---
+++ BookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using BookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options)
+        {
+            
+        }
+        public DbSet <Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Cups", DisplayOrder = "1" },
+                new Category { Id = 2, Name = "Plates", DisplayOrder = "2" }
+                );
+        }
+    }
+}
